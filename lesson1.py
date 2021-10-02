4742f0144206af483b0a0d405ff8e96cec7466a8
example <makeawish.baby@gmail.com>
2021-10-02 17:35:23 +0300

    final with while lesson3.py

--- lesson1.py
+++ lesson1.py
@@ -124,10 +124,162 @@
 #     if (symbol.lower() not in "eyuioa") and symbol.isalpha() and symbol.isupper():
 #         print(symbol)
 
-str1 = "I'm Qwerty"
+# str1 = "I'm Qwerty"
 # for symbol in str1:
 #     print(f"symbol '{symbol}' --> {ord(symbol)}")
 
-for index in range(ord("A"), ord('z')+ 1, 2):
-    print(f"index: {index} --> '{chr(index)}'")
+# for index in range(ord("A"), ord('z')+ 1, 2):
+#     print(f"index: {index} --> '{chr(index)}'")
 
+# count = 0
+# while count < 10:
+#     print("while loop", count)
+#     count += 1
+
+# count = 0
+# do_loop = True
+# while do_loop:
+#     print("while loop", count)
+#     count += 1
+#     if count >= 10:
+#         do_loop = False
+
+# count = 0
+# while True:
+#     print("while loop", count)
+#     count += 1
+#     if count >= 10:
+#         break
+
+# from random import randint
+# min_limit = 1
+# max_limit = 100
+# comp_value = randint(min_limit, max_limit)
+# cur_value = int(input(f"Угадай число от {min_limit} до {max_limit}: "))
+# go_game = True
+# while go_game:
+#     if cur_value > comp_value:
+#         cur_value = int(input("Попробуй число меньше: "))
+#     elif cur_value < comp_value:
+#         cur_value = int(input("Попробуй число больше: "))
+#     else:
+#         go_game = False
+#         print("Ты победил")
+
+# from random import randint
+# min_limit = 1
+# max_limit = 100
+# comp_value = randint(min_limit, max_limit)
+# cur_value = int(input(f"Угадай число от {min_limit} до {max_limit}: "))
+# while cur_value != comp_value:
+#     case_word = "меньше" if cur_value > comp_value else "больше"
+#     cur_value= int(input(f"Попробуй число {case_word}"))
+#     # if cur_value > comp_value:
+#     #     cur_value = int(input("Попробуй число меньше: "))
+#     # else:
+#     #     cur_value = int(input("Попробуй число больше: "))
+# print("Ты победил")
+
+# value = input("Введи целое число: ")
+# try:
+#     value_int = int(value)
+#     result = 1 / value_int
+#     print(result)
+# except ValueError:
+#     print("Это не число")
+# except ZeroDivisionError:
+#     print("Делить на 0 нельзя")
+
+# my_str = "qwerty 123 %(^ SGS"
+# for symbol in my_str:
+#     if not symbol.isalnum() and symbol != " ":
+#         print(symbol)
+
+# my_str = "qwerty 123 %(^ AGS"
+# for symbol in my_str:
+#     if symbol.lower() in "eyuioa":
+#         print(symbol)
+
+# my_tuple = (1, -10, "qwe", True, 3.14, (-2, 0), ["a", "z"])
+# my_list = [1, -10, "qwe", True, 3.14, (-2, 0), ["a", "z"]]
+
+# print(my_tuple, type(my_tuple))
+# print(my_list, type(my_list))
+# index = -1
+# print(my_tuple[index], my_list[index])
+# print(len(my_tuple), len(my_list))
+
+# new_list = my_list[1:-1]
+# print(new_list)
+#
+# new_tuple = my_tuple[::-1]
+# print(new_tuple)
+
+# for value in my_list:
+#     if type(value) == int:
+#         print(value)
+
+# my_tuple = (1, -10, "qwe", True, 3.14, (-2, 0), ["a", "z"])
+# my_list = [1, -10, "qwe", True, 3.14, (-2, 0), ["a", "z"]]
+# my_list[0] = 100
+# print(my_list)
+
+# my_tuple = (1, -10, "qwe", True, 3.14, (-2, 0), ["a", "z"])
+# val1, *val = my_tuple
+# print(val1, val)
+
+# my_list = [1, (-2, 0), ["a", "z"]]
+# my_list[-1][0] = 100
+# print(my_list)
+
+# my_tuple = (1, (-2, 0), ["a", "z"])
+# my_tuple[-1][0] = 100
+# print(my_tuple)
+
+# my_table = [[1, 2], [3, 4]]
+# my_table[1][1] = 24
+# print(my_table)
+
+# my_tuple = (1, 2, 3)
+# print(id(my_tuple), id(1))
+# my_tuple = (100, *my_tuple[1:])
+# print(id(my_tuple))
+
+# list_1 = [1, 2]
+# list_2 = [3, 4]
+# new_list = [list_1.copy(), list_2]
+# print(new_list)
+# list_1[0] = 100
+# list_2[0] = 300
+# print(new_list, list_1)
+
+# value = [10, 20]
+# new_list = [value] * 5
+# value[0] = 100
+# print(new_list)
+
+# new_list = []
+# for symbol in "qwerty":
+#     new_list.append(symbol)
+# new_list.append(1000)
+# print(new_list)
+# result = new_list.pop(5)
+# print(result)
+
+# new_list = list("qwerty")
+# print(new_list)
+
+# new_list = list("new_tuple")
+# print(new_list)
+# new_str = "$".join(new_list)
+# print(new_str)
+
+filename = "lesson1.py.txt"
+
+##  filename = filename.replace(".txt", "")
+
+# filename_parts = filename.split(".")
+# filename = ".".join(filename_parts[:-1])
+
+filename = filename.rsplit(".", 1)[0]  # задать вопрос
+print(filename)
